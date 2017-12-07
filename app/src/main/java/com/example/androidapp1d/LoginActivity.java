39c7dffdff6081a2d4efb3b902b46d2c1aaace7f
cheryl <cherylgohqp@users.noreply.github.com>
2017-12-07 22:30:21 +0800

    Did a temporary layout of the login page (radio buttons direct to respective prof/student page)

--- app/src/main/java/com/example/androidapp1d/LoginActivity.java
+++ app/src/main/java/com/example/androidapp1d/LoginActivity.java
@@ -3,20 +3,46 @@ package com.example.androidapp1d;
 import android.content.Intent;
 import android.os.Bundle;
 
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+
+import static android.R.attr.orientation;
 
 public class LoginActivity extends AppCompatActivity {
     private Button loginbutton;
     private Button tempbutton;
+    private RadioButton student;
+    private RadioButton prof;
+    private Button enter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.loginactivity);
         loginbutton = (Button) findViewById(R.id.loginbutton);
+        student = (RadioButton) findViewById(R.id.student);
+        prof = (RadioButton) findViewById(R.id.prof);
+        enter = (Button) findViewById(R.id.enterapp);
 
+        enter.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (student.isChecked()) {
+                    Intent intent = new Intent(LoginActivity.this, StudFeedActivity.class);
+                    startActivity(intent);
+                } else if (prof.isChecked()) {
+                    Intent intent = new Intent(LoginActivity.this, ProfFeedActivity.class);
+                    startActivity(intent);
+                }
+
+            }
+        });
+
+/*
         loginbutton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -33,5 +59,6 @@ public class LoginActivity extends AppCompatActivity {
                 startActivity(profActivity);
             }
         });
+    }*/
     }
 }
